38f5e0f870aba95bca6df3cda59eaaee8c1e742c
Sasidhar Donaparthi <sasidhar.donaparthi@gmail.com>
2021-09-04 22:55:38 +0530

    added interactive app for plotly click events and table selection events

---
+++ my_interactive_app/app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+from streamlit_plotly_events import plotly_events
+import plotly.express as px
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode
+from table_configuration import get_table_configuration
+
+import plotly.graph_objects as go
+import pandas as pd
+
+st.set_page_config(layout='wide')
+
+@st.cache
+def get_data():
+    df = px.data.iris()
+    return df
+
+df = get_data()
+
+if 'selected_points' not in st.session_state:
+    st.session_state['selected_points'] = []
+
+st.title("Iris Data")
+col1, col2 = st.columns((4,2))
+colors = list(df['species'].drop_duplicates())
+#st.write(colors)
+
+#
+color_discrete_map={
+                "versicolor": "red",
+                "setosa": "green",
+                "virginica": "magenta",}
+
+fig = px.scatter(
+    df, x="sepal_width", y="sepal_length",color="species", symbol="species",
+    color_discrete_map=color_discrete_map,
+)
+with col1:
+    selected_points = plotly_events(fig, click_event=True,select_event=False, hover_event=False,override_height=800)
+if selected_points:
+    for selected_point in selected_points:
+        if selected_point not in st.session_state['selected_points']:
+            st.session_state['selected_points'].append(selected_point)
+            print(selected_point)
+            #print(df_temp.iloc[selected_point['pointIndex']])
+
+with col2:
+    data = []
+    for point in st.session_state['selected_points']:
+        color = colors[int(point['curveNumber'])]
+        records = df[df.species==color].iloc[[point['pointIndex']]].to_dict("records")
+        data.extend(records)
+    df_selected = pd.DataFrame(data)
+    grid_options = get_table_configuration(df_selected)
+    st.subheader("Selected Data")
+    grid_response = AgGrid(
+        df_selected,
+        gridOptions = grid_options,
+        height = 500,
+        width = "100%",
+        data_return_mode = list(DataReturnMode.__members__)[0],
+        update_mode = list(GridUpdateMode.__members__)[6],
+        fit_columns_on_grid_load = False,
+        allow_unsafe_jscode= False,
+        enable_enterprise_modules = False
+    )
+print(list(DataReturnMode.__members__))
+print(list(GridUpdateMode.__members__))
+print(grid_response['selected_rows'])
+if grid_response['selected_rows']:
+    with col2:
+        st.write(grid_response['selected_rows'])
+
---
+++ my_interactive_app/table_configuration.py
@@ -0,0 +1,43 @@
+from st_aggrid import GridOptionsBuilder
+
+def get_table_configuration(df):
+    table_editable = False
+    enable_selection = True
+    selection_mode = "single" # ['single','multiple']
+    use_checkbox = False
+    rowMultiSelectWithClick = False
+    suppressRowDeselection = False
+    enable_pagination = True
+    paginationAutoSize = True
+    paginationPageSize = 25
+    groupSelectsChildren = False
+    groupSelectsFiltered = False
+
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=table_editable)
+    gb.configure_selection(selection_mode)
+
+    if ((selection_mode == 'multiple') & (not use_checkbox)):
+        if not rowMultiSelectWithClick:
+            suppressRowDeselection = False
+        else:
+            suppressRowDeselection=False
+
+    if not paginationAutoSize:
+        paginationPageSize = 25
+    if enable_selection:
+        gb.configure_selection(selection_mode)
+        if use_checkbox:
+            gb.configure_selection(selection_mode, use_checkbox=True, groupSelectsChildren=groupSelectsChildren, groupSelectsFiltered=groupSelectsFiltered)
+        if ((selection_mode == 'multiple') & (not use_checkbox)):
+            gb.configure_selection(selection_mode, use_checkbox=False, rowMultiSelectWithClick=rowMultiSelectWithClick, suppressRowDeselection=suppressRowDeselection)
+
+    if enable_pagination:
+        if paginationAutoSize:
+            gb.configure_pagination(paginationAutoPageSize=True)
+        else:
+            gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=paginationPageSize)
+
+    gb.configure_grid_options(domLayout='normal')
+    gridOptions = gb.build()
+    return gridOptions
